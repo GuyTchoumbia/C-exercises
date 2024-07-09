5707636606734a32383d013581b46bd922a74f24
GuyTchoumbia <guy.tchoumbia@gmail.com>
2024-07-09 12:12:47 +0200

    renamed files for brevity

---
+++ sandbox.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
+#include "utils.h"
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    printf("x : ");
+    intToBinary(x);    
+    
+    return 0;
+}
+
