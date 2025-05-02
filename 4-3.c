1a12e174219ab554c7c9347bae6cdebfaadae252
Guy Tchoumbia <guy.tchoumbia@gmail.com>
2025-05-02 02:03:09 +0200

    4-3 done

--- 4-3.c
+++ 4-3.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h> /* for atof() */
+#include <ctype.h>
 
-#define MAXOP   100 /* max size of opernad or operator */
+#define MAXOP   100 /* max size of operand or operator */
 #define NUMBER  '0' /* signal that a number was found */
+#define BUFSIZE 100
+#define MAXVAL  100     /* maximum depth of val stack */
 
 int getop(char []);
 void push(double);
 double pop(void);
+int getch(void);
+void ungetch(int);
+
+/* external variables for pop and push*/
+int sp = 0;             /* next free stack position*/
+double val[MAXVAL];     /* value stack*/
+/* external variables for getch/ungetch */
+char buf[BUFSIZE];      /* buffer for ungetch */
+int bufp = 0;           /* next free position in buf */
 
 /* reverse polish calculator */
 int main()
 {
     int type;
-    double operand2;
+    double operand1, operand2;
     char s[MAXOP];
 
     while ((type = getop(s)) != EOF)
@@ -26,7 +38,7 @@ int main()
                 push(pop() + pop());
                 break;
             case '*':
-                push(pop() + pop());
+                push(pop() * pop());
                 break;
             case '-':
                 operand2 = pop();
@@ -43,6 +55,18 @@ int main()
                     printf("error: zero divisor\n");
                 }
                 break;
+            // 4-3 add % operator
+            case '%':
+                operand2 = pop();
+                if (operand2 != 0.0)
+                {
+                    push((int) pop() % (int) operand2);
+                }
+                else
+                {
+                    printf("error: zero divisor\n");
+                }
+                break;
             case '\n':
                 printf("\t%.8g\n", pop());
                 break;
@@ -53,11 +77,6 @@ int main()
     }
     return 0;
 }
-
-#define MAXVAL  100     /* maximum depth of val stack */
-
-int sp = 0;             /* next free stack position*/
-double val[MAXVAL];     /* value stack*/
 
 /* push : push f onto value stack */
 void push(double f)
@@ -77,7 +96,7 @@ double pop(void)
 {
     if (sp > 0)
     {
-        return val[sp--];
+        return val[--sp];
     }
     else{
         printf("error: stack empty\n");
@@ -85,47 +104,66 @@ double pop(void)
     }
 }
 
-#include <ctype.h>
-
-int getch(void);
-void ungetch(int);
-
 /* getop: get next operator or numeric operand */
 int getop(char s[])
 {
-    int i, c;
+    int i, ch, sign, next;
 
-    while ((s[0] = c = getch()) == ' ' || c == '\t');
+    while ((s[0] = ch = getch()) == ' ' || ch == '\t'); // skip whitespace
 
-    s[1] = '\0';
-    if (!isdigit(c) && c != '.')
+    s[1] = '\0'; // need to close the string at this point to test for isdigit
+    if (!isdigit(ch) && ch != '.' && ch != '-')
     {
-        return c;   /* not a number */
+        return ch;   /* not a number */
     }
     i = 0;
-    if (isdigit(c))  /* collect inger part */
+    // 4-3: negative
+    // use s[i] as a buffer
+    if (ch == '-')
     {
-        while   (isdigit(s[++i] = c = getch()));
+        // if space after, its an minus operator
+        if ((next = getch()) == ' ' || s[i] == '\t')
+        {
+            return ch;
+        }
+        // if digit or decimal sign behind, its a negative sign
+        else if (isdigit(next) || next == '.')
+        {
+            s[i] = ch;
+            ch = next;
+            ungetch(next);
+        }
+    }
+    if (isdigit(ch))  /* collect integer part */
+    {
+        while (isdigit(s[++i] = ch = getch()))
+        {
+            ;
+        }
+    }
+    if (ch == '.')
+    {
+        while (isdigit(s[++i] = ch = getch()))
+        {
+            ;
+        }
     }
     s[i] = '\0';
-    if (c != EOF)
+    if (ch != EOF)
     {
-        ungetch(c);
+        ungetch(ch);
     }
     return NUMBER;
 }
 
-#define BUFSIZE     100
-
-char buf[BUFSIZE];      /* buffer for ungetch */
-int bufp = 0;           /* next free position in buf */
-
-int getch(void)         /* get a (possibly pushed back) character */
+/* get a (possibly pushed back) character */
+int getch(void)       
 {
     return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
-void ungetch(int c)      /* push character back on input */
+/* push character back on input */
+void ungetch(int ch)
 {
     if (bufp >= BUFSIZE)
     {
@@ -133,6 +171,6 @@ void ungetch(int c)      /* push character back on input */
     }
     else
     {
-        buf[bufp++] = c;
+        buf[bufp++] = ch;
     }
 }
